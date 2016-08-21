720c7c74363414f03f4693094ebd8c8fa44a9f95
tdi-digital <gustavo@tdi.digital>
2016-08-21 03:07:18 -0300

    Update otp.sh

--- opt/otp.sh
+++ opt/otp.sh
@@ -1,7 +1,8 @@
 export PATH=/app/otp/bin:bin:/usr/bin:/bin
-echo "xxxxxx"
-echo /*
+echo "bin:"
+echo /bin/*
+echo "opt:"
+echo /opt/*
 echo /app/*
 echo /app/otp/*
-cd /app
-erl -pa /app/ebin -run tumbleweed start
+erl -pa /app/otp/lib -pa /app/ebin -run tumbleweed start
